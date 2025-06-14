057c7c6d515a96337efe14f3df8bbbd84cbd5faf
132905 <125987008+132905@users.noreply.github.com>
2025-06-14 20:26:18 +0530

    Update PortfolioHome.jsx

--- src/pages/PortfolioHome.jsx
+++ src/pages/PortfolioHome.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import { Github, Linkedin, Mail, Download } from "lucide-react";
 import { motion } from "framer-motion";
 
